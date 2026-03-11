c31fcbe371d878b919c49df8a620a46edaa32d44
destroyersyt829-dotcom <destroyersyt829@gmail.com>
2026-03-11 15:03:47 +0530

    Update ModuleManager.java

--- src/main/java/net/raphimc/immediatelyfast/module/ModuleManager.java
+++ src/main/java/net/raphimc/immediatelyfast/module/ModuleManager.java
@@ -17,104 +17,108 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class ModuleManager implements ButtonListener {
-	private final List<Module> modules = new ArrayList<>();
+    private final List<Module> modules = new ArrayList<>();
 
-	public ModuleManager() {
-		addModules();
-		addKeybinds();
-	}
+    public ModuleManager() {
+        addModules();
+        addKeybinds();
+    }
 
-	public void addModules() {
-		//Combat
-		add(new AimAssist());
-		add(new AnchorMacro());
-		add(new AutoCrystal());
-		add(new AutoDoubleHand());
-		add(new AutoHitCrystal());
-		add(new AutoInventoryTotem());
-		add(new TriggerBot());
-		add(new AutoPot());
-		add(new AutoPotRefill());
-		add(new AutoWTap());
-		add(new CrystalOptimizer());
-		add(new DoubleAnchor());
-		add(new HoverTotem());
-		add(new NoMissDelay());
-		add(new ShieldDisabler());
-		add(new TotemOffhand());
-		add(new AutoJumpReset());
-		add(new MaceTrigger());
-		add(new AutoMace());
-		add(new MaceSwap());
-		//Misc
-		add(new Prevent());
-		add(new AutoXP());
-		add(new NoJumpDelay());
-		add(new PingSpoof());
-		add(new FakeLag());
-		add(new AutoClicker());
-		add(new KeyPearl());
-		add(new NoBreakDelay());
-		add(new Freecam());
-		add(new PackSpoof());
-		add(new Sprint());
+    public void addModules() {
+        //Combat
+        add(new AimAssist());
+        add(new AnchorMacro());
+        add(new AutoCrystal());
+        add(new AutoDoubleHand());
+        add(new AutoHitCrystal());
+        add(new AutoInventoryTotem());
+        add(new TriggerBot());
+        add(new AutoPot());
+        add(new AutoPotRefill());
+        add(new AutoWTap());
+        add(new CrystalOptimizer());
+        add(new DoubleAnchor());
+        add(new HoverTotem());
+        add(new NoMissDelay());
+        add(new ShieldDisabler());
+        add(new TotemOffhand());
+        add(new AutoJumpReset());
+        add(new MaceTrigger());
+        add(new AutoMace());
+        add(new MaceSwap());
 
-		//Render
-		add(new HUD());
-		add(new NoBounce());
-		add(new PlayerESP());
-		add(new StorageEsp());
-		add(new TargetHud());
+        //Misc
+        add(new Prevent());
+        add(new AutoXP());
+        add(new NoJumpDelay());
+        add(new PingSpoof());
+        add(new FakeLag());
+        add(new AutoClicker());
+        add(new KeyPearl());
+        add(new NoBreakDelay());
+        add(new Freecam());
+        add(new PackSpoof());
+        add(new Sprint());
 
-		//Client
-		add(new ClickGUI());
-		add(new Friends());
-		add(new SelfDestruct());
-	}
+        //Render
+        add(new HUD());
+        add(new NoBounce());
+        add(new PlayerESP());
+        add(new StorageEsp());
+        add(new TargetHud());
 
-	public List<Module> getEnabledModules() {
-		return modules.stream()
-				.filter(Module::isEnabled)
-				.toList();
-	}
+        //Client
+        add(new ClickGUI());
+        add(new Friends());
+        add(new SelfDestruct());
+    }
 
+    public List<Module> getEnabledModules() {
+        return modules.stream()
+                .filter(Module::isEnabled)
+                .toList();
+    }
 
-	public List<Module> getModules() {
-		return modules;
-	}
+    public List<Module> getModules() {
+        return modules;
+    }
 
-	public void addKeybinds() {
-		Argon.INSTANCE.getEventManager().add(ButtonListener.class, this);
+    public void addKeybinds() {
+        Argon.INSTANCE.getEventManager().add(ButtonListener.class, this);
 
-		for (Module module : modules)
-			module.addSetting(new KeybindSetting(EncryptedString.of("Keybind"), module.getKey(), true).setDescription(EncryptedString.of("Key to enabled the module")));
-	}
+        for (Module module : modules)
+            module.addSetting(new KeybindSetting(EncryptedString.of("Key"), -1, module));
+    }
 
-	public List<Module> getModulesInCategory(Category category) {
-		return modules.stream()
-				.filter(module -> module.getCategory() == category)
-				.toList();
-	}
+    public List<Module> getModulesInCategory(Category category) {
+        return modules.stream()
+                .filter(module -> module.getCategory() == category)
+                .toList();
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T extends Module> T getModule(Class<T> moduleClass) {
-		return (T) modules.stream()
-				.filter(moduleClass::isInstance)
-				.findFirst()
-				.orElse(null);
-	}
+    @SuppressWarnings("unchecked")
+    public <T extends Module> T getModule(Class<T> moduleClass) {
+        return (T) modules.stream()
+                .filter(moduleClass::isInstance)
+                .findFirst()
+                .orElse(null);
+    }
 
-	public void add(Module module) {
-		modules.add(module);
-	}
+    public void add(Module module) {
+        modules.add(module);
+    }
 
-	@Override
-	public void onButtonPress(ButtonEvent event) {
-		if(!SelfDestruct.destruct) {
-			modules.forEach(module -> {
-				if(module.getKey() == event.button && event.action == GLFW.GLFW_PRESS)
-					module.toggle();
-			});
-		}
-	}
-}
+    public void save() {
+        ConfigManager.save(modules);
+    }
+
+    @Override
+    public void onButtonPress(ButtonEvent event) {
+        if (!SelfDestruct.destruct) {
+            modules.forEach(module -> {
+                if (module.getKey() == event.button && event.action == GLFW.GLFW_PRESS)
+                    module.toggle();
+            });
+        }
+    }
+			}
